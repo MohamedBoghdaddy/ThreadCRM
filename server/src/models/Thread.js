571d2665421ca5b1b89260d4b60fad261ec7f1b4
MohamedBoghdaddy <mohamedhammad3.142@gmail.com>
2026-01-05 16:25:19 -0800

    Add server/src/models/Thread.js with Mongoose thread schema

---
+++ server/src/models/Thread.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const threadSchema = new mongoose.Schema(
+  {
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
+    lastMessageAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Thread', threadSchema);
